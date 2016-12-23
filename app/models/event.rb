40d04d2c29dbe571427ebe0838c32ea1d4ef3853
Mert Guldur <mertguldur@gmail.com>
2016-12-23 18:13:13 +0300

    Add a chronological start/end dates check for any time and all day events

--- app/models/event.rb
+++ app/models/event.rb
@@ -54,6 +54,10 @@ class Event < ApplicationRecord
   end
 
   def chonological_start_and_end_times
-    errors.add(:base, "End time must be later than start time") if end_time <= start_time
+    if specific_time?
+      errors.add(:base, "End time must be later than start time") if end_time <= start_time
+    else
+      errors.add(:base, "End date can't be earlier than start date") if end_time < start_time
+    end
   end
 end
