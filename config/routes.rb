3e73d3949a05363655e88164ce7b749059fd4abd
Mert Guldur <mertguldur@gmail.com>
2017-01-05 18:26:26 +0300

    Remove 'resources :users'

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
   post   '/signin',  to: 'sessions#create'
   delete '/signout', to: 'sessions#delete'
 
-  get '/signup',    to: 'users#new'
+  get '/signup',    to: 'users#new',    as: 'new_user'
   post '/users',    to: 'users#create', as: 'create_user'
   get '/users/:id', to: 'users#show',   as: 'show_user'
   put '/users/:id', to: 'users#update', as: 'update_user'
-  resources :users
 
   get 'events',        to: 'events#index',  as: 'events'
   get 'events/new',    to: 'events#new',    as: 'new_event'
@@ -18,16 +17,20 @@ Rails.application.routes.draw do
   post 'events',       to: 'events#create', as: 'create_event'
   delete 'events/:id', to: 'events#delete', as: 'delete_event'
 
-  get 'apps', to: 'app_authorization_requests#index', as: 'app_authorization_requests'
+  get 'apps',  to: 'app_authorization_requests#index',   as: 'app_authorization_requests'
   post 'apps', to: 'app_authorization_responses#create', as: 'create_app_authorization_response'
 
   namespace :api do
     namespace :v1 do
       get 'status', to: 'status#index'
 
-      get '/users/:user_id/events', to: 'events#index'
-
       post 'authorize_app', to: 'app_authorization_requests#create'
+
+      get '/users/:user_id/events',  to: 'events#index'
+      post '/users/:user_id/events', to: 'events#create'
+      get '/events/:id',             to: 'events#show'
+      put '/events/:id',             to: 'events#update'
+      delete '/events/:id',          to: 'events#delete'
     end
   end
 end
