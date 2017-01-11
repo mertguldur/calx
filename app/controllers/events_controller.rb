0c283b9fd171daa0a47b72d3c5c0ec8a7b732e5a
Mert Guldur <mertguldur@gmail.com>
2017-01-11 17:43:42 +0300

    Remove unnecessary event_type_id assignment in EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -64,9 +64,6 @@ class EventsController < ApplicationController
     permitted[:end_time] = combine_date_and_time(permitted[:end_date], permitted[:end_time])
     permitted.delete(:end_date)
 
-    permitted[:event_type_id] = EventType[permitted[:event_type]].id
-    permitted.delete(:event_type)
-
     permitted[:user_id] = current_user.id
     permitted
   end
